5cf80668889b7acafdd99ba24100cf9b781fc43e
Samuelonoja <Samuelonoja970@gmail.com>
2021-09-05 04:15:09 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useLayoutEffect, useState } from "react";
+import Head from "next/head";
+import Greeting from "../components/greeting";
+import axios from "axios";
+
+export default function Home() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [text, setText] = useState<string>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      getGreeting();
+    }, 2000);
+  }, []);
+
+  async function getGreeting() {
+    setLoading(true);
+    try {
+      await axios.get("/api/hello-world").then((res) => {
+        setText(res.data.message);
+        setLoading(false);
+        console.log(res);
+      });
+    } catch (error) {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Progigs - Remote gigs just for you</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="My new title" key="title" />
+      </Head>
+      <div className="flex h-screen w-full justify-center items-center align-center">
+        <Greeting text={text ? text : "loading..."} />
+      </div>
+    </>
+  );
+}
